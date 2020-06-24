6dad1c8358f7fab528a94d82b6b80fe789aec4e6
Matthieu Druaud <matthieu.druaud@gmail.com>
2020-06-24 22:26:23 +0100

    /

--- pathfinder_exec.py
+++ pathfinder_exec.py
@@ -6,7 +6,3 @@ from pathfinder import ui
 if __name__ == "__main__":
     ui.main()
 
-# if __name__ == "__main__":
-#     sys.argv[0] = re.sub(r"(-script\.pyw?|\.exe)?$", "", sys.argv[0])
-#     sys.exit(ui.main())
-
