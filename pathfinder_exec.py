9b8a4da109e95694f606f72ed909464b67963866
Matthieu Druaud <matthieu.druaud@gmail.com>
2020-06-24 21:09:11 +0100

    /

--- pathfinder_exec.py
+++ pathfinder_exec.py
@@ -6,8 +6,6 @@ from pathfinder import ui
 if __name__ == "__main__":
     ui.main()
 
-# syntaxe pour executer un script dans le terminal avec des arguments
-
 # if __name__ == "__main__":
 #     sys.argv[0] = re.sub(r"(-script\.pyw?|\.exe)?$", "", sys.argv[0])
 #     sys.exit(ui.main())
